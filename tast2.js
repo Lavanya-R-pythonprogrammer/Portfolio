136730726ee9bc89ada38c4b8165ce810dc6c117
Lavanya-R-pythonprogrammer <lavanyar5500@gmail.com>
2024-09-13 15:17:31 +0530

    Add files via upload

---
+++ tast2.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    const icons=document.querySelector(".icon");
+    menu.classList.toggle("open");
+    icons.classList.toggle("open");
+
+
+}
